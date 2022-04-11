eee9d5f4430fff0170a6cf5974fe667dcd6e90fa
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-11 21:32:00 +0530

    msg

---
+++ AllConversion.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+
+{
+    int decision;
+    cout<<"\t **WELCOME TO MY PROGRAM FOR NUMBER SYSTEM CONVERSION** \n";
+    cout<<"Which conversion you want to do ? select any one:-\n1.Binary to Decimal\n2.Decimal to binary\n3.Binary to Octal\n4.Octal to Bianary\n5.Binary to Hexadecimal\n6.Hexadecimal to Binary\n7.Decimal to octal\n8.Octal to decimal\n9.Decimal to Hexadecimal\n10.Hexadecimal to Decimal\n";
+    cin>>decision;
+    if (decision==1)
+    {
+        bin2dec(bin);
+    }
+    else if (decision==2)
+    {
+        
+    }
+    
+    
+    
+
+    return 0;
+
+}
