1bfea091a00d17d94f6315496a10a010637ddd49
Manav Khadka <manavkhadka2004@gmail.com>
2022-04-11 22:12:23 +0530

    msg1

--- AllConversion.cpp
+++ AllConversion.cpp
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long l;
+int bin2dec(l bin)
+{
+    int base=1,rem,dec=0;
+    while (bin!=0)
+    {
+        rem=bin%10;
+        bin/=10;
+        dec+=base*rem;
+        base*=2;
+    }
+    return dec;
+
+}
+void dec2bin(l dec)
+{
+   int a[30];
+   int i=0;
+   while (dec>0)
+   {
+       a[i]=dec%2;
+       dec/=2;
+       i++;
+   }
+   for (int j = i-1; j >=0; j--)
+   {
+       cout<<a[j];
+   }
+   
+   
+
+ 
+    
+
+}
 
 int main()
 
@@ -10,12 +45,81 @@ int main()
     cin>>decision;
     if (decision==1)
     {
-        bin2dec(bin);
+        l bin;
+        cout<<"enter binary value:";
+        cin>>bin;
+        
+        cout<<"The  decimal of binary is :"<<bin2dec(bin)<<"\n";
     }
     else if (decision==2)
     {
+        l dec;
+        cin>>dec;
+      dec2bin(dec);
+      cout<<"\'n";
         
     }
+    // else if (decision==3)
+    // {
+    //     l bin;
+    //     cin>>bin;
+        
+    //     cout<<bin2oct(bin);
+    // }
+    // else if (decision==4)
+    // {
+    //     l oct;
+    //     cin>>oct;
+    //    cout<<oct2bin(oct);
+    // }
+    
+    // else if (decision==5)
+    // {
+    //     l bin;
+    //     cin>>bin;
+    //     cout<<bin2hex(bin);
+    // }
+    
+    // else if (decision==6)
+    // {
+    //     l hex;
+    //     cin>>hex;
+    //     cout<<hex2bin(hex);
+        
+    // }
+    
+    // else if (decision==7)
+    // {
+    //     l dec;
+    //     cin>>dec;
+    //     cout<<dec2oct(dec);
+    // }
+    
+    // else if (decision==8)
+    // {
+    //     l oct;
+    //     cin>>oct;
+    //     cout<<oct2dec(oct);
+        
+    // }
+    
+    // else if (decision==9)
+    // {
+    //     l dec;
+    //     cin>>dec;
+    //     cout<<dec2hex(dec);
+    // }
+    
+    // else if (decision==10)
+    // {
+    //     l hex;
+    //     cin>>hex;
+    //     cout<<hex2dec(hex);
+        
+    // }
+    
+    
+    
     
     
     
